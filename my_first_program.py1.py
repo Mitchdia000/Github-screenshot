7c639d595f8ce7299815c4e7fb359d0d7fc56a5d
Mitchdia000 <dmitchell199@ivytech.edu>
2025-12-07 14:57:28 -0500

    Add files via upload

---
+++ my_first_program.py1.py
@@ -0,0 +1,27 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> Pint("Hello World")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    Pint("Hello World")
+NameError: name 'Pint' is not defined. Did you mean: 'int'?
+>>> print(Hello World")
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print("Hello World")
+...       
+Hello World
+>>> 
+======== RESTART: C:/Users/diamo/OneDrive/Documents/my_first_program.py ========
+Hello World
+I just wrote my first Python program
+This is so much fun
+>>> c = "cats"
+...       
+>>> d = "dogs"
+...       
+>>> s = "Its raining " + c + " and " + d + " . "
+...       
+>>> print(s)
+...       
+Its raining cats and dogs . 
